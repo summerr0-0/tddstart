fa95361a46d7327bb265cb9c4592d3585f2cdbe1
summerr0-0 <summerr060@gmail.com>
2023-07-06 22:43:15 +0900

    first init

---
+++ src/main/kotlin/com/example/tddstart/TddstartApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tddstart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddstartApplication
+
+fun main(args: Array<String>) {
+    runApplication<TddstartApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tddstart/TddstartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tddstart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddstartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
